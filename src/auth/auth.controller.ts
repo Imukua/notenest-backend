305408d01be2fd4dcc03d8039f2e805bc01f90a5
Imukua <ianmukuaa@gmail.com>
2024-10-16 18:47:39 +0300

    Add: login controller and guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthPayloadDto } from './dto/auth.dto';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalGuard } from './guards/local.guard';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    @Post('login')
-    login(@Body() authpayload: AuthPayloadDto ) {
-        return 'login';
-    }
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  @UseGuards(LocalGuard)
+  login(@Req() req: Request) {
+    return req.user;
+  }
+
 }
