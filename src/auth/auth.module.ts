c216c367163528b500f83017b265bc723edf34be
Imukua <ianmukuaa@gmail.com>
2024-10-16 18:48:23 +0300

    Added: strategies for passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { Auth } from './auth';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    JwtModule,
+  ],
   controllers: [AuthController],
-  providers: [Auth, AuthService]
-})
+  providers: [AuthService, LocalStrategy, JwtStrategy]
+}) 
 export class AuthModule {}
