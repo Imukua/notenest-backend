d795e8881f3af57b5401625bf3178cf80c89720d
Imukua <ianmukuaa@gmail.com>
2024-10-17 12:13:55 +0300

    Added: neccessary imoports

--- src/journals/journals.module.ts
+++ src/journals/journals.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JournalsService } from './journals.service';
 import { JournalsController } from './journals.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot(),
+    AuthModule,
+  ],
   providers: [JournalsService],
   controllers: [JournalsController]
 })
