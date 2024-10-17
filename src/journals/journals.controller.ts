7aa1d4bbc4bd0b6a588948217f743d82293c11f0
Imukua <ianmukuaa@gmail.com>
2024-10-17 14:37:57 +0300

    Added: update journal functionality

--- src/journals/journals.controller.ts
+++ src/journals/journals.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { JournalsService } from './journals.service';
 import { CreateJournalDto } from './dto/create.journal.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { JwtPayload } from 'src/auth/strategies/jwt.strategy';
+import { UpdateJournalDto } from './dto/update.journal.dto';
 
 
 interface Request {
@@ -18,5 +19,15 @@ export class JournalsController {
         const user = req.user
        return this.journalsService.createJournalEntry(user.id, body)
     }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch(':id')
+    update(@Param("id") id:string, @Req() req: Request, @Body() body: UpdateJournalDto) {
+        console.log(id)
+        const user = req.user
+       return this.journalsService.updateJournalEntry(user.id,id, body)
+    }
+
+
 
 }
--- src/journals/journals.service.ts
+++ src/journals/journals.service.ts
@@ -1,8 +1,9 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { create } from 'domain';
 import { use } from 'passport';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateJournalDto } from './dto/create.journal.dto';
+import { UpdateJournalDto } from './dto/update.journal.dto';
 
 @Injectable()
 export class JournalsService {
@@ -23,4 +24,39 @@ export class JournalsService {
             }
         })   
     }
+
+
+    async updateJournalEntry(userId: string, journalId: string, data: UpdateJournalDto) {
+
+        const existingEntry = await this.prismaService.journalEntry.findUnique({
+            where: {
+                id: journalId
+            }
+        })
+
+        if(!existingEntry) {
+            throw new HttpException('Journal entry not found', 404)
+        }
+
+        if (existingEntry.userId !== userId) {
+            throw new NotFoundException(`You do not have permission to update this journal entry`);
+          }
+    
+        const {title, content, category} = data;
+        if(!title || !content || !category) {
+            throw new HttpException('Missing required fields', 400)
+        }
+
+        return this.prismaService.journalEntry.update({
+            where: { id: journalId },
+            data: {
+              title: data.title,
+              content: data.content,
+              category: data.category,
+            },
+          });
+   
+    }
+
+  
 }
