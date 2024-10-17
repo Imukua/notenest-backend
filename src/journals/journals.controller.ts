c5cdd71741e33112bb997c1d5b260fec76b45ff3
Imukua <ianmukuaa@gmail.com>
2024-10-17 13:41:51 +0300

    Added: post method for create journal and its service

--- src/journals/journals.controller.ts
+++ src/journals/journals.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { JournalsService } from './journals.service';
+import { CreateJournalDto } from './dto/create.journal.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { JwtPayload } from 'src/auth/strategies/jwt.strategy';
 
+
+interface Request {
+    user: JwtPayload;
+}
 @Controller('journals')
-export class JournalsController {}
+export class JournalsController {
+    constructor(private readonly journalsService: JournalsService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post('create')
+    create(@Req() req: Request, @Body() body: CreateJournalDto) {
+        const user = req.user
+       return this.journalsService.createJournalEntry(user.id, body)
+    }
+
+}
--- src/journals/journals.service.ts
+++ src/journals/journals.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { create } from 'domain';
+import { use } from 'passport';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateJournalDto } from './dto/create.journal.dto';
 
 @Injectable()
-export class JournalsService {}
+export class JournalsService {
+    constructor(private prismaService: PrismaService) {}
+
+    async createJournalEntry(userId: string, data: CreateJournalDto) {
+        const {title, content, category} = data;
+        if(!title || !content || !category) {
+            throw new HttpException('Missing required fields', 400)
+        }
+
+        return this.prismaService.journalEntry.create({
+            data: {
+                title,
+                content,
+                category,
+                userId
+            }
+        })   
+    }
+}
