945fdf9c4c11832583a6ee40d310f80143be9ec1
Imukua <ianmukuaa@gmail.com>
2024-10-20 22:57:55 +0300

    Refactor: Update CORS origin configuration

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const process_1 = require("process");
+const allowedOrigins = process_1.env.CORS_ORIGIN ? process_1.env.CORS_ORIGIN.split(',') : [];
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: false,
+        origin: allowedOrigins,
         credentials: true,
     });
     await app.listen(3000);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
+
+const allowedOrigins = env.CORS_ORIGIN ? env.CORS_ORIGIN.split(',') : [];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: false,
+    origin: allowedOrigins,
     credentials: true, 
   });
   await app.listen(3000);
