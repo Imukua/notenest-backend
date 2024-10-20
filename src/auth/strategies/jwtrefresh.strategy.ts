896632f5b9b7b025bc1459fe42ae7669a720cd60
Imukua <ianmukuaa@gmail.com>
2024-10-20 13:11:18 +0300

    Refactor: Update JwtRefreshStrategy to validate refresh token in request header
    - Added `PrismaService` dependency to `JwtRefreshStrategy` constructor
    - Modified `validate` method to extract refresh token from request header
    - Queried the database to find the user with the provided refresh token
    - Threw an `UnauthorizedException` if the user or refresh token is invalid
    - Returned the user's id and username if the refresh token is valid

--- src/auth/strategies/jwtrefresh.strategy.ts
+++ src/auth/strategies/jwtrefresh.strategy.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service'; // Adjust based on your project structure
+import { Request } from 'express';
 
 export interface JwtPayload {
   id: string;
@@ -9,18 +11,36 @@ export interface JwtPayload {
   iat?: number;
   exp?: number;
 }
-
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwtrefresh') {
-  constructor( private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: false, 
       secretOrKey: configService.get('JWT_REFRESH_TOKEN_SECRET'),
+      passReqToCallback: true, 
     });
   }
 
-  validate(payload: JwtPayload) {
-    return { id: payload.id, username: payload.username };
+  async validate(req: Request, payload: JwtPayload) {
+    const refreshToken = req.get('Authorization').replace('bearer ', '');
+
+    const userWithToken = await this.prisma.user.findUnique({
+      where: { id: payload.id },
+      include: {
+        refreshTokens: {
+          where: { token: refreshToken }, 
+        },
+      },
+    });
+
+    if (!userWithToken || userWithToken.refreshTokens.length === 0) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+
+    return { id: userWithToken.id, username: userWithToken.username };
   }
 }
