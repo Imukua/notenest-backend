c5674eda5142448ccc01b6b6fb88ce2b6e782015
Imukua <ianmukuaa@gmail.com>
2024-10-20 16:00:39 +0300

    Refactor: Add directUrl environment variable to Prisma datasource configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// Load environment variables based on NODE_ENV
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production.local' : '.env.development.local';
+dotenv.config({ path: envFile });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5000',
+    origin: process.env.CORS_ORIGIN,
     credentials: true, 
   });
   await app.listen(3000);
