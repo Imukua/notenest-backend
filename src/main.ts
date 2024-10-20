12c8d834f7770cac7f5596d3cb293bc151c45eb6
Imukua <ianmukuaa@gmail.com>
2024-10-20 16:31:30 +0300

    Refactor: Remove dotenv package and environment variable configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-// Load environment variables based on NODE_ENV
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production.local' : '.env.development.local';
-dotenv.config({ path: envFile });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
